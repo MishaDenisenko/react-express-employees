b38b438784dfc9ec33616a6298ac1ef72b1d552a
mihailden <midenisenko@gmail.com>
2023-11-25 21:39:45 +0200

    add paths and types

---
+++ client/src/constants/paths.ts
@@ -0,0 +1,10 @@
+export const PATHS = {
+    home: '/',
+    employee: '/employee',
+    employeeAdd: '/employee/add',
+    employeeEdit: '/employee/edit',
+    status: '/status',
+    login: '/login',
+    register: '/register',
+
+} as const;
