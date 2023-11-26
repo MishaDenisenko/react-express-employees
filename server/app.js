25abd26347d4e570de2223bb5ce0d70feb585c1a
mihailden <midenisenko@gmail.com>
2023-11-27 00:16:41 +0200

    add cors

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const {userRouter, employeeRouter} = require('./routes');
-
+const cors = require('cors');
 require('dotenv').config()
+const {userRouter, employeeRouter} = require('./routes');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use('/api/user', userRouter);
